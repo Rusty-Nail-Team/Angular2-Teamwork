761efd5058fa1a9b731eb80e083f3383a5097829
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-02 21:13:37 +0200

    add-snapshots/add-snapshots.component.html 30%

--- src/app/app_pages/adventure/add-snapshots/add-snapshots.component.ts
+++ src/app/app_pages/adventure/add-snapshots/add-snapshots.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AdventureDataCreateModel } from '../../../app_core/models/adventure-data-create.model';
+
 @Component({
   selector: 'app-add-snapshots',
   templateUrl: './add-snapshots.component.html',
   styleUrls: ['./add-snapshots.component.css']
 })
 export class AddSnapshotsComponent implements OnInit {
+private count:number;
 
-  constructor() { }
+  constructor() { 
+    this.count = 1;
+  }
 
   ngOnInit() {
   }
