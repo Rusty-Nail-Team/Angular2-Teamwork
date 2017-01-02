334da4a026aff812b4669abc6fab8cde07b2e7bc
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-02 21:54:05 +0200

    add-snapshots.component.html 40%

--- src/app/app_pages/adventure/add-snapshots/add-snapshots.component.ts
+++ src/app/app_pages/adventure/add-snapshots/add-snapshots.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AdventureDataCreateModel } from '../../../app_core/models/adventure-data-create.model';
+import { UserProfileModel } from './../../../app_core/models/user-profile.model';
+
+
 
 @Component({
   selector: 'app-add-snapshots',
@@ -9,12 +12,19 @@ import { AdventureDataCreateModel } from '../../../app_core/models/adventure-dat
 })
 export class AddSnapshotsComponent implements OnInit {
 private count:number;
+private model: AdventureDataCreateModel;
+private currentUser: UserProfileModel;
 
   constructor() { 
-    this.count = 1;
+    this.count = 1
   }
 
   ngOnInit() {
+  }
+
+  addAdventureImage() {
+    this.model.ownerId = this.currentUser.objectId;
+    
   }
 
 }
