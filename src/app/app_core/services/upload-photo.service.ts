a944defa313b791bd6d4320ebec4437cc4b102d9
Borayvor <Boray@abv.bg>
2017-01-02 15:28:12 +0200

    add upload-photo.service.ts

--- src/app/app_core/services/upload-photo.service.ts
+++ src/app/app_core/services/upload-photo.service.ts
@@ -10,21 +10,21 @@ export class UploadPhotoService {
   private adventureTitlePhotofilesUrl: string;
 
   constructor(private data: BaseService) {
-    this.usersPhotofilesUrl = '/files/media/usersPhotos';
-    this.adventureDataPhotofilesUrl = '/files/media/adventurePhotos';
-    this.adventureTitlePhotofilesUrl = '/files/media/adventureTitlePhoto';
+    this.usersPhotofilesUrl = '/files/media/usersPhotos/';
+    this.adventureDataPhotofilesUrl = '/files/media/adventurePhotos/';
+    this.adventureTitlePhotofilesUrl = '/files/media/adventureTitlePhoto/';
   }
 
-  uploadUserPhoto() {
-
+  uploadUserPhoto(file: File) {
+    return this.data.post(this.usersPhotofilesUrl + file.name + '?overwrite=true', file);
   }
 
-  uploadAdventureTitlePhoto() {
-
+  uploadAdventureTitlePhoto(file: File) {
+    return this.data.post(this.usersPhotofilesUrl + file.name + '?overwrite=true', file);
   }
 
-  uploadAdventureDataPhoto() {
-
+  uploadAdventureDataPhoto(file: File) {
+    return this.data.post(this.usersPhotofilesUrl + file.name + '?overwrite=true', file);
   }
 
 }
