922e9df041bc43324a393a128b683d5083260bf7
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-02 14:00:52 +0200

    About page 50% its ready.

--- src/app/app_pages/about/about.component.ts
+++ src/app/app_pages/about/about.component.ts
@@ -6,12 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  private title: string;
+  private titles: string[];
+  private address: string;
+  private time: number;
+  private contactEmail: string;
+  private tel: number;
 
   constructor() { }
 
   ngOnInit() {
-    this.title = 'About';
+    this.titles = ['About', 'Address', 'Hours of operation', 'Contact info'];
+    this.address = 'бул. "Александър Малинов“ 31,1729 София';
+    this.time = 8;
+    this.contactEmail = 'academy@telerik.com';
+    this.tel = 888888888;
   }
 
 }
